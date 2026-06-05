e3d2f9516e469ac57800da1ff5b22d57534d0834
JPriskomatis <JPriskomatis@gmail.com>
2026-06-05 14:28:52 +0300

    Add: Random events alongside monsterEvents

--- Assets/Scripts/UI/Newspaper/NewspaperDaily.cs
+++ Assets/Scripts/UI/Newspaper/NewspaperDaily.cs
@@ -5,10 +5,18 @@ public class NewspaperDaily : MonoBehaviour
 {
     [SerializeField] private NewDayManager dayManager;
 
+    [Header("Event 1")]
     [SerializeField] private TextMeshProUGUI eventTitle;
     [SerializeField] private TextMeshProUGUI eventDescription;
 
+    [Header("Event 2")]
+    [SerializeField] private TextMeshProUGUI eventTitle1;
+    [SerializeField] private TextMeshProUGUI eventDescription2;
+
     [SerializeField] private TextMeshProUGUI currentDayNum;
+
+    [SerializeField] private WorldEventSO[] randomWorldEvent;
+    private WorldEventSO currentRandomEvent;
 
     private void Start()
     {
@@ -24,5 +32,17 @@ public class NewspaperDaily : MonoBehaviour
         eventTitle.text = currentEvent.title;
         eventDescription.text = currentEvent.description;
         currentDayNum.text = "Day "+dayManager.CurrentDay.Value.ToString();
+
+        GetRandomEvent();
+        eventTitle1.text = currentRandomEvent.title;
+        eventDescription2.text = currentRandomEvent.description;
+
+
+    }
+
+    private void GetRandomEvent()
+    {
+        int randomInx = Random.Range(0, randomWorldEvent.Length);
+        currentRandomEvent = randomWorldEvent[randomInx];
     }
 }
