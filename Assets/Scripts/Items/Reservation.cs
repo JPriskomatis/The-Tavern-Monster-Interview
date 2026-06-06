79022447e7c124eb1c225e7fda6bd48847314c69
JPriskomatis <JPriskomatis@gmail.com>
2026-06-06 14:44:47 +0300

    Add: UI Fixups and reservation is now an event rather than a StringEvent

--- Assets/Scripts/Items/Reservation.cs
+++ Assets/Scripts/Items/Reservation.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Reservation : InteractableItem
@@ -6,12 +7,17 @@ public class Reservation : InteractableItem
     private string customerNames = "test";
 
     private string customerType = "test";
-    public StringGameEvent ReservationUI;
+    public GameEvent ReservationUI;
+
+    [SerializeField] TextMeshProUGUI npcName, npcType;
 
     protected override void BeginInteraction()
     {
         Debug.Log("Reserved Table for: "+ customerNames);
-        ReservationUI.Raise(customerNames + " " + customerType);
+
+        npcName.text = customerNames;
+        npcType.text = customerType;
+        ReservationUI.Raise();
     }
 
     public void ApplyReservation(string names, string types)
